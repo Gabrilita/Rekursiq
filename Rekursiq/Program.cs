46a46c49f98b4a8610f8ac91eb0da1b5298ff195
Gabrilita <96725781+Gabrilita@users.noreply.github.com>
2023-05-17 12:05:13 +0300

    Add files via upload

---
+++ Rekursiq/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rekursiq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int n = 3;
+            //for (int i1 = 1; i1 <= n; i1++)
+            //{
+            //    for (int i2 = 1; i2 <= n; i2++)
+            //    {
+            //        for (int i3 = 1; i3 <= n; i3++)
+            //        {
+            //            Console.WriteLine($"{i1} {i2} {i3}");
+            //        }          
+            //    }
+            //}
+            int n = 3;
+            int k = 2;
+            for (int i1 = 1; i1 <=n; i1++)
+            {
+                for (int i2 = i1; i2 <= n; i2++)
+                {
+                    Console.WriteLine($"{i1} {i2}");
+                }
+            }
+        }
+    }
+}
